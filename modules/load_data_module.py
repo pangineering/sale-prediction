7b6d7b573597081abe1fc99c74df1c6c0fd0405a
pantkdkrt <p.engineering6415@gmail.com>
2022-05-29 20:22:05 +0700

    add python files

---
+++ modules/load_data_module.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+class Load_train_data:
+    pass
+
+class Load_data:
+    pass
---
+++ modules/visualise_modules.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+
+## For visualization
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+class Visualise_Data:
+    pass
